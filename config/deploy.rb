7ca480031b9572302fdc56e5e6792d7fc7fa1ad6
Nemermel <nemermel18@gmail.com>
2016-06-19 01:48:22 +0300

    watwatwat

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server '185.25.116.244', port: 3000, roles: [:web, :app, :db], primary: true
+server '185.25.116.244', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:Nemermel/office-build.git'
 set :application,     'office-build'
