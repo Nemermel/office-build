93ddf6ff2fbf720dd9038b8e9162f05f7ca0ddb8
Nemermel <nemermel18@gmail.com>
2016-07-07 17:09:45 +0300

    Fixed back-end bug in comments

--- db/migrate/20160706150721_add_admin_to_users.rb
+++ db/migrate/20160706150721_add_admin_to_users.rb
@@ -1,6 +1,7 @@
 class AddAdminToUsers < ActiveRecord::Migration
   def change
     User.create(:name => "admin", :password => "q6Ah23e1D", :email => "admin@admin.com")
+    User.create(:name => "44CentreUser", :password => "e7aH55ty2")    
   end
   # def self.up
   #   User.create(:name => "admin", :password => "admin", :email => "admin@admin.com")
